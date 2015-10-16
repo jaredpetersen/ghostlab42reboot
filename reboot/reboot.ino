ff7c823ff3464bf8e2c79ddd334439ac1572c4bf
Jared Petersen <jpetersen11@wou.edu>
2015-10-15 18:38:03 -0700

    Added printing prototype

--- reboot/reboot.ino
+++ reboot/reboot.ino
@@ -3,8 +3,9 @@
  */
 
 #include "Wire.h"
-#define IS31FL3730_DIGIT_4_I2C_ADDRESS 0x63 // each I2C has a unique bus address, the 4 digit IS31FL3730 display is 0x63
-#define IS31FL3730_DIGIT_6_I2C_ADDRESS 0x60 // each I2C has a unique bus address, the 6 digit IS31FL3730 display is 0x60
+// Each I2C has a unique bus address
+#define IS31FL3730_DIGIT_4_I2C_ADDRESS 0x63 // 4 digit IS31FL3730 display
+#define IS31FL3730_DIGIT_6_I2C_ADDRESS 0x60 // 6 digit IS31FL3730 display
 
 const byte IS31FL3730_Data_Registers = 0x01; // "Matrix 1 Data Register" index in the IS31FL3730
 // 8 bit value to define which segments are lit. 0 == off, 1 == on
@@ -12,44 +13,46 @@ const byte IS31FL3730_Data_Registers = 0x01; // "Matrix 1 Data Register" index i
 // Works great for data, but stop after the matrix data is updated and then end the command.
 
 const byte IS31FL3730_Update_Column_Register = 0x0C; // "Update Column Register" index in the IS31FL3730
-// The data sent to the Data Registers will be stored in temporary registers. 
-// A write operation of any 8-bit value to the Update Column Register is required to update 
+// The data sent to the Data Registers will be stored in temporary registers.
+// A write operation of any 8-bit value to the Update Column Register is required to update
 // the Data Registers (01h~0Bh, 0Eh~18h)
 
 const byte IS31FL3730_Lighting_Effect_Register = 0x0D; // "Lighting Effect Register" index in the IS31FL3730
-// Bottom four bits control the Full Current Setting for Each Row Output 
-//   0000: 40mA 
-//   0001: 45mA 
-//   0010: 50mA 
-//   0011: 55mA 
-//   0100: 60mA 
-//   0101: 65mA 
-//   0110: 70mA 
-//   0111: 75mA 
-//   1000: 5mA 
-//   1001: 10mA 
-//   1010: 15mA 
-//   1011: 20mA 
-//   1100: 25mA 
-//   1100: 30mA 
+// Bottom four bits control the Full Current Setting for Each Row Output
+//   0000: 40mA
+//   0001: 45mA
+//   0010: 50mA
+//   0011: 55mA
+//   0100: 60mA
+//   0101: 65mA
+//   0110: 70mA
+//   0111: 75mA
+//   1000: 5mA
+//   1001: 10mA
+//   1010: 15mA
+//   1011: 20mA
+//   1100: 25mA
+//   1100: 30mA
 //   1110: 35mA
 //   1111: ???? - not documented!
 
 const byte IS31FL3730_PWM_Register = 0x19; // "PWM Register" index in the IS31FL3730
 // The PWM Register can modulate LED light with 128 different items.
-// When the D7 set to “1”, the PWM is the 128 item. 
+// When the D7 set to “1”, the PWM is the 128 item.
 // When the D7 set to “0”, D6:D0 set the PWM from the 0 item to the 127 item.
 
 const byte IS31FL3730_Reset_Register = 0xFF; // "Reset Register" index in the IS31FL3730
-// Once user writes any 8-bit data to the Reset Register, IS31FL3730 will reset all registers to default value.
-// On  initial power-up, the IS31FL3730 registers are reset to their default values for a blank display.
+// Once user writes any 8-bit data to the Reset Register, IS31FL3730 will reset
+// all registers to default value.
+// On  initial power-up, the IS31FL3730 registers are reset to their default
+// values for a blank display.
 
 /*
  * Android Setup Function
  */
 void setup() {
   Wire.begin();
-  
+
   // Set the maximum display power for both displays
   setDisplayPowerMax(4);
   setDisplayPowerMax(6);
@@ -57,21 +60,14 @@ void setup() {
 
 /*
  * Resets the display and sets the current to the maximum (5mA per segment)
- * 
+ *
  * Parameters:
- * Digits Selects the display to dim, 4 = 4 digit display and all others = 6 digit display
+ * digits Selects the display to set the current, 4 = 4 digit display and all
+ *        others = 6 digit display
  */
-void resetDisplay(int Digits) 
+void resetDisplay(int digits)
 {
-  // Set up the wire transmission depending on the display being reset
-  if (Digits == 4)
-  { 
-    Wire.beginTransmission(IS31FL3730_DIGIT_4_I2C_ADDRESS); 
-  }
-  else
-  { 
-    Wire.beginTransmission(IS31FL3730_DIGIT_6_I2C_ADDRESS); 
-  }
+  setupWireTransmission(digits);
 
   // Reset the lighting effects (dim, bright, etc.)
   // Send any value to reset the lighting (value ignored)
@@ -80,178 +76,82 @@ void resetDisplay(int Digits)
   Wire.endTransmission();
 
   // Reset the current
-  setDisplayPowerMax(Digits);
+  setDisplayPowerMax(digits);
 }
-
-/*
- * Sets the current to the minimum (5mA per segment)
- * 
- * Parameters:
- * Digits Selects the display to dim, 4 = 4 digit display and all others = 6 digit display
- */
-void setDisplayPowerMin(int Digits) 
-{
-  // Set up the transmission depending on the display being reset
-  if (Digits == 4)
-  { 
-    Wire.beginTransmission(IS31FL3730_DIGIT_4_I2C_ADDRESS); 
-  }
-  else
-  { 
-    Wire.beginTransmission(IS31FL3730_DIGIT_6_I2C_ADDRESS); 
-  }
-  
-  Wire.write(IS31FL3730_Lighting_Effect_Register);
-  Wire.write(0x08); // Lowest level, 10mA
-  Wire.endTransmission();
-}  
-
-/*
- * Sets the current to the maximum alllowed for these displays (0mA per segment)
- * 
- * Parameters:
- * Digits Selects the display to dim, 4 = 4 digit display and all others = 6 digit display
- */
-void setDisplayPowerMax(int Digits) {
-  /* The display driver does allow currents greater than the displays should take - do not allow tanything over 20mA!
-   * The display driver resets to 40mA per segment which is too much.  Somehow we need to make sure that is never
-   * violated but the part is write-only, so we need to keep track of the setting somewhere or just always force to the max current.
-   */
-
-  // Set up the transmission depending on the display being reset
-  if (Digits == 4)
-  { 
-    Wire.beginTransmission(IS31FL3730_DIGIT_4_I2C_ADDRESS); 
-  }
-  else
-  { 
-    Wire.beginTransmission(IS31FL3730_DIGIT_6_I2C_ADDRESS); 
-  }
-  
-  Wire.write(IS31FL3730_Lighting_Effect_Register);
-  Wire.write(0x0B); // Highest level, 20mA
-  Wire.endTransmission();
-}  
 
 /**
  * Dims the display
- * 
+ *
  * Parameters:
- * Digits Selects the display to dim, 4 = 4 digit display and all others = 6 digit display
- * PWM    Sets the dimming level, not very linear. 0x80 = full intensity, 0x00 is off
+ * digits Selects the display to set the current, 4 = 4 digit display and all
+ *        others = 6 digit display
+ * PWM    Sets the dimming level, not very linear. 0x80 = full intensity,
+ *        0x00 is off
  */
-void setDisplayBrightness (int Digits, byte PWM) {
-  // Probably should rewrite to allow a user setting in percentage, so an integer and 0 to 100 map to 0 to 128.  
-  // Anything above 100 would be clamped to the max useful register value of 128 (0x80).
+void setDisplayBrightness (int digits, byte PWM) {
+  // TODO Allow user setting in percentage with 100% being the useful register
+  // value of 128 (0x80)
 
-  // Set up the transmission depending on the display being reset
-  if (Digits == 4)
-  { 
-    Wire.beginTransmission(IS31FL3730_DIGIT_4_I2C_ADDRESS); 
-  }
-  else
-  { 
-    Wire.beginTransmission(IS31FL3730_DIGIT_6_I2C_ADDRESS); 
-  }
+  setupWireTransmission(digits);
 
   // Begin dimming the display
   Wire.write(IS31FL3730_PWM_Register); // Lighting Effect Register
 
   // Do not allow the user to set values higher than the max allowed (always on)
   if (PWM < 0x80)
-  { 
+  {
     // Value is less than the max, set the requested value
     Wire.write(PWM); // write the value that was requested
   }
   else
-  { 
+  {
     // Value is higher than the max, set the max
     Wire.write(0x80); // write the max value allowed (always on)
   }
-  
+
   Wire.endTransmission();
 }
 
-
 /*
- * Test Routine to force something interesting on the 4-digit display
+ * Prints the characters to the selected display. The only characters allowed
+ * are numbers 0-9 and letters A, b, C, d, E, and F
  */
-void display_update_4a() {
+void print(int digits, char value[])
+{ 
+  // User can technically give us any digit, so we have to do a nice
+  // conversion with that data so that we can use it for proper iteration
+  if (digits != 4)
+  {
+    digits = 6;
+  }
+
   // Write the display data in the temporary registers
-  Wire.beginTransmission(IS31FL3730_DIGIT_4_I2C_ADDRESS);
-  Wire.write(IS31FL3730_Data_Registers); // Matrix 1 Data registers
-  Wire.write(0x3F); // Digit 1 value (far left) -- Display "0"
-  Wire.write(0x5B); // Digit 2 value -- Display "2"
-  Wire.write(0x4F); // Digit 3 Value -- Display "3"
-  Wire.write(0x66); // Digit 4 value (far right) -- Display "4"
-  Wire.endTransmission();
+  setupWireTransmission(digits);
+  Wire.write(IS31FL3730_Data_Registers);
   
-  // Transfer the display data from the temporary registers to the display registers (display the values)
-  Wire.beginTransmission(IS31FL3730_DIGIT_4_I2C_ADDRESS);
-  Wire.write(IS31FL3730_Update_Column_Register); // Update Column Register
-  Wire.write(0x00); // Send any value to initate the display (value ignored)
-  Wire.endTransmission();
-}
+  // Iterate over the print value and print out the individual characters
+  // Any string that goes over the number of digits gets cut off
+  // Any string that goes under the number of digits has blank spaces in
+  // in the remaining spots
+  for (int i = 0; i < digits; i++)
+  {
+    Wire.write(charToDisplayByte(value[i]));
+  }
 
-/*
- * Test Routine to force something interesting on the 4-digit display
- */
-void display_update_4b() {
-  // Write the display data in the temporary registers
-  Wire.beginTransmission(IS31FL3730_DIGIT_4_I2C_ADDRESS);
-  Wire.write(IS31FL3730_Data_Registers); // Matrix 1 Data Registers 
-  Wire.write(0xFF); // Digit 1 value (far left)
-  Wire.write(0x02); // Digit 2 value
-  Wire.write(0x04); // Digit 3 value
-  Wire.write(0x08); // Digit 4 value (far right)
+  // End the temporary register transmission
   Wire.endTransmission();
-  
-  // Transfer the display data from the temporary registers to the display registers (display the values)
-  Wire.beginTransmission(IS31FL3730_DIGIT_4_I2C_ADDRESS);
-  Wire.write(IS31FL3730_Update_Column_Register); // Update Column Register
-  Wire.write(0x00); // Send any value to initate the display (value ignored)
-  Wire.endTransmission();
-}
 
-/*
- * Test Routines to force something interesting on the 6-digit display
- */
-void display_update_6a() {
-  Wire.beginTransmission(IS31FL3730_DIGIT_6_I2C_ADDRESS);
-  Wire.write(IS31FL3730_Data_Registers); // Matrix 1 Data Registers 
-  Wire.write(0x06); // Digit 1 value (far left)
-  Wire.write(0x5B); // Digit 2 value
-  Wire.write(0x4F); // Digit 3 value
-  Wire.write(0x66); // Digit 4 value
-  Wire.write(0x6D); // Digit 5 value
-  Wire.write(0x7D); // Digit 6 value (far right)
-  Wire.endTransmission();
+  // Transfer the display data from the temporary registers to the display
+  setupWireTransmission(digits);
   
-  // Now transfer the data from the temporary registers to the display registers
-  Wire.beginTransmission(IS31FL3730_DIGIT_6_I2C_ADDRESS);
-  Wire.write(IS31FL3730_Update_Column_Register); // Update Column Register
-  Wire.write(0x00); // Any value should work (value ignored)
-  Wire.endTransmission();
-}
+  // Write to the Update Column Register to let the board know we want to
+  // update the display
+  Wire.write(IS31FL3730_Update_Column_Register); 
+  
+  // Send any value to initate the display (value ignored)
+  Wire.write(0x00);
 
-/*
- * Test Routines to force something interesting on the 6-digit display
- */
-void display_update_6b() {
-  Wire.beginTransmission(IS31FL3730_DIGIT_6_I2C_ADDRESS);
-  Wire.write(IS31FL3730_Data_Registers); // Matrix 1 Data Registers 
-  Wire.write(0xFF); // Digit 1 value (far left)
-  Wire.write(0x02); // Digit 2 value
-  Wire.write(0x04); // Digit 3 value
-  Wire.write(0x08); // Digit 4 value
-  Wire.write(0x10); // Digit 5 value
-  Wire.write(0x20); // Digit 6 value (far right)
-  Wire.endTransmission();
-  
-  // Now transfer the data from the temporary registers to the display registers
-  Wire.beginTransmission(IS31FL3730_DIGIT_6_I2C_ADDRESS);
-  Wire.write(IS31FL3730_Update_Column_Register); // Update Column Register
-  Wire.write(0x00); // Any value should work (value ignored)
+  // End the Updte Column Register Transmission
   Wire.endTransmission();
 }
 
@@ -259,32 +159,103 @@ void display_update_6b() {
  * Arduino Overall Looping Function
  */
 void loop() {
-  // Print "0234" and hold
-  display_update_4a();
-  
-  // Wait and print "12345678"
+  print(4, "01");
   delay(500);
-  setDisplayBrightness(6,0x80);
-  display_update_6a();
-  
+  print(6, "HE110");
   delay(500);
-  
-  setDisplayBrightness(4,0x10);
-  display_update_4b();
-  
+  resetDisplay(4);
   delay(500);
-
-  setDisplayBrightness(6,0x02);
-  display_update_6b();
-  
-  delay(500);
-
-  setDisplayBrightness(4,0x80);
-  resetDisplay(4); // reset the 4 digit display
-  
-  delay(500);
-  
-  resetDisplay(6); // reset the 6 digit display
-  
+  resetDisplay(6);
   delay(500);
 }
+
+
+/******************************************************************************
+ *                           Arduino Helper Methods                           *
+ ******************************************************************************/
+
+/*
+ * Sets the current to the minimum (5mA per segment)
+ *
+ * Parameters:
+ * digits Selects the display to set the current, 4 = 4 digit display and all
+ *        others = 6 digit display
+ */
+void setDisplayPowerMin(int digits)
+{
+  setupWireTransmission(digits);
+
+  Wire.write(IS31FL3730_Lighting_Effect_Register);
+  Wire.write(0x08); // Lowest level, 10mA
+  Wire.endTransmission();
+}
+
+/*
+ * Sets the current to the maximum allowed for these displays (0mA per segment)
+ *
+ * Parameters:
+ * digits Selects the display to set the current, 4 = 4 digit display and all
+ *        others = 6 digit display
+ */
+void setDisplayPowerMax(int digits) {
+  // The display driver does allow currents greater than the displays should
+  // take - do not allow anything over 20mA!
+  // The display driver resets to 40mA per segment which is too much.  Somehow
+  // we need to make sure that is never violated but the part is write-only, so
+  // we need to keep track of the setting somewhere or just always force to the
+  // max current.
+
+  setupWireTransmission(digits);
+
+  Wire.write(IS31FL3730_Lighting_Effect_Register);
+  Wire.write(0x0B); // Highest level, 20mA
+  Wire.endTransmission();
+}
+
+/*
+ * Set up the Wire transmission depending on the display being used
+ */
+void setupWireTransmission(int digits)
+{
+  if (digits == 4)
+  {
+    // Use the four digit display
+    Wire.beginTransmission(IS31FL3730_DIGIT_4_I2C_ADDRESS);
+  }
+  else
+  {
+    // Use the six digit display
+    Wire.beginTransmission(IS31FL3730_DIGIT_6_I2C_ADDRESS);
+  }
+}
+
+/*
+ * Converts characters into the appropriate bytes for display (gfedcba format)
+ */
+byte charToDisplayByte(char displayCharacter)
+{
+  // Although this isn't very time efficient (linear time), it at least saves 
+  // us precious space that a HashMap/Dictionary (constant time) would have 
+  // gobbled up
+  if (displayCharacter == '0') return 0x3F;
+  else if (displayCharacter == '1') return 0x06;
+  else if (displayCharacter == '2') return 0x5B;
+  else if (displayCharacter == '3') return 0x4F;
+  else if (displayCharacter == '4') return 0x66;
+  else if (displayCharacter == '5') return 0x6D;
+  else if (displayCharacter == '6') return 0x7D;
+  else if (displayCharacter == '7') return 0x07;
+  else if (displayCharacter == '8') return 0x7F;
+  else if (displayCharacter == '9') return 0x6F;
+  // The characters are only able to displayed in one case (see left side)
+  // but we should be forgiving and let the user input the wrong case
+  else if ((displayCharacter == 'A') || (displayCharacter == 'a')) return 0x77;
+  else if ((displayCharacter == 'b') || (displayCharacter == 'B')) return 0x7C;
+  else if ((displayCharacter == 'C') || (displayCharacter == 'c')) return 0x39;
+  else if ((displayCharacter == 'd') || (displayCharacter == 'D')) return 0x5E;
+  else if ((displayCharacter == 'E') || (displayCharacter == 'e')) return 0x79;
+  else if ((displayCharacter == 'F') || (displayCharacter == 'f')) return 0x71;
+  // Anythin else turns into a blank for that character
+  else { return NULL; }
+}
+
