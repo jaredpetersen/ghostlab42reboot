82a03a18840809549a1a7caa3512f0e6f825ee39
Doug Keithley <DougKeithley@GhostLab42.com>
2016-05-22 13:46:57 -0700

    Modified counting example to better emulate movie trailers

--- examples/ex5_counting/ex5_counting.ino
+++ examples/ex5_counting/ex5_counting.ino
@@ -6,24 +6,36 @@ GhostLab42Reboot reboot;
 void setup()
 {
   reboot.begin();
+  
+  // Turn down the brightness of the main displays
+  reboot.setDisplayBrightness(0, 60);
+  reboot.setDisplayBrightness(1, 100);
+  reboot.setDisplayBrightness(2, 60);
 }
 
 void loop()
 {
   String countStr;
+  int count = 2087;
   
   for (int i = 0; i < 1000; i++)
   {
-    // Count up with leading 0's
-    countStr = "00000" + String(i);
-    reboot.write(0, countStr.substring(countStr.length() - 6));
+    // Count down
+    reboot.write(0, String(120999 - i));
 
-    // Count down slowly
-    reboot.write(1, String(5000 - i/23));
+    // Count up with leading 0s really fast
+    countStr = "000" + String(16 * i);
+    reboot.write(1, countStr.substring(countStr.length() - 4));
     
-    // Count up with a negative sign very slowly
-    reboot.write(2, String(-300 + i/80));
+    // Show a number that is about 2087 but moves around randomly by a few counts
+    // Slow the update down so it changes more slowly
+    if ((i % 30) == 0)
+    {
+      count = 2087 + random(-2, 3);
+    }
+    
+    reboot.write(2, String(count));
 
-    delay(40);  
+    delay(30);  
   }
 }
